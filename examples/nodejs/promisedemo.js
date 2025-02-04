949b9fbcf1a29f18b19a955c1117dd84cc0fe3d9
Stephen Schaub <sschaub@sermonaudio.com>
2025-02-04 11:36:19 -0500

    Updates

--- examples/nodejs/promisedemo.js
+++
@@ -1,7 +0,0 @@
-let fs = require('fs')
-
-
-let data = await fs.readFile('promisedemo.js');
-
-
-console.log(data)
--- examples/nodejs/rest_client/demo.js
+++ examples/nodejs/rest_client/demo.js
@@ -1,27 +1,25 @@
 // demo.js
 // To use this demo:
-//   npm install request
 //   npm install -g json-server
 //   json-server --watch db.json
 //   node demo.js
 
-let request = require('request');
-
 let options = {
-  url: 'http://localhost:3000/posts',
   headers: {
     'Content-Type': 'application/json'
   }
 };
 
-request(options, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    result = JSON.parse(body);
-    result.forEach(function(item) {
+fetch('http://localhost:3000/posts', options).then(response => {
+  if (response.status != 200) {
+    throw new Error(`Received status code ${response.status}`)
+  }
+  return response.json()
+}).then(data => {
+    data.forEach(item => {
         console.log(item.id + ": " + item.message);
     });
-  } else {
-      console.log(error);
-      console.log(response.statusCode);
-  }
-});
+}).catch(error => {
+  console.error(`Error: ${error}`)
+
+})
